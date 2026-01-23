0874359f1b4ce8ee1b3ac59ab853ef6f9003e362
mongdmin <tmdals0429@pusan.ac.kr>
2026-01-23 16:20:08 +0900

    9095, 15988 code

---
+++ 9095_123더하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+T=int(input())
+
+for _ in range(T):
+    n=int(input())
+    dp=[0]*(n+1)
+    if n>=1: dp[1]=1
+    if n>=2: dp[2]=2
+    if n>=3: dp[3]=4
+    for i in range(4,n+1): dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+    print(dp[n])
